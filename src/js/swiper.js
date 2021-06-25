4aaf6ccf46d25c85c5d0d3241fada6ee2ebc1fe8
Vadim Solovei <vadims.olovei90@gmail.com>
2021-06-25 20:36:47 +0300

    make slider responsive

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,27 +1,33 @@
-import Swiper, { Pagination, Navigation } from 'swiper';
+import Swiper, { Pagination, Navigation } from "swiper";
 
 Swiper.use([Pagination, Navigation]);
 
-const swiper = new Swiper('.swiper-container', {
-  // Optional parameters
+const swiper = new Swiper(".swiper-container", {
   loop: true,
   slidesPerView: 1,
   spaceBetween: 20,
   observer: true,
   observeParents: true,
   navigation: {
-    prevEl: '.swiper-button-prev-custom',
-    nextEl: '.swiper-button-next-custom',
+    prevEl: ".slider--button_prev",
+    nextEl: ".slider--button_next",
   },
   pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
+    el: ".swiper-pagination",
+    type: "bullets",
     clickable: true,
   },
+  grabCursor: true,
   breakpoints: {
     768: {
-      slidesPerView: 4,
+      slidesPerView: 2,
       spaceBetween: 16,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+    1300: {
+      slidesPerView: 4,
     },
   },
 });
